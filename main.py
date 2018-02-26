ef26ddfee671ad8aa400f30fcc083f5872c16ffd
Vasily <gerasimovv1990@gmail.com>
2018-02-26 14:29:09 +0300

    changes in main.py - html inside

--- main.py
+++ main.py
@@ -2,7 +2,37 @@ import webapp2
 
 class HelloWebapp2(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello, webapp2!')
+        self.response.write('''<!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8" content="text/html">
+            <title>Just a page</title>
+            <style>
+            div {
+              color: red;
+              background-color: black;
+              display: inline-block;
+            }
+
+            .but:hover {
+              color: blue;
+              background-color: yellow;
+              cursor: pointer;
+            }
+            p:hover {
+                height: 10px;
+            }
+            </style>
+          </head>
+          <body>
+            <div>
+              <p>
+                Some text
+              </p>
+              <input type='submit' class="but" name="go!" value="Vtudeeeee!">
+            </div>
+          </body>
+        </html>''')
 
 app = webapp2.WSGIApplication([
     ('/', HelloWebapp2),
