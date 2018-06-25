2df36e4b12c5f6667db493bcc2eb994ab1359cc6
Vasiliy <gerasimovv1990@gmail.com>
2018-06-25 20:50:10 +0300

    continue django-recall/website

--- django-recall/website/music/admin.py
+++ django-recall/website/music/admin.py
@@ -6,4 +6,3 @@ from .models import Album, Song
 
 admin.site.register(Album)
 admin.site.register(Song)
-# Register your models here.
--- django-recall/website/music/views.py
+++ django-recall/website/music/views.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
-from django.http import Http404
 
 def index(request):
     all_albums = Album.objects.all()
@@ -11,8 +10,16 @@ def index(request):
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404('Album does not exist')
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message': 'You did not select a valid song'})
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
